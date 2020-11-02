9e06c9ebb1c2ff8e9dde462ec9049a1a20b7f9a6
Anish Nesarkar <anish9461@gmail.com>
2020-11-01 23:19:33 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,3 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+  source = "github.com/terraform-aws-anish/s/s3"
 }
